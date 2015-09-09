25a599f5b15e42a31de531f1051f9d3dbd355445
Eugene Sokolov <esguardian@outlook.com>
2015-09-09 17:12:22 +0300

    move payload parsing from SQL Select to script body

--- usr/local/bin/accrep.py
+++ usr/local/bin/accrep.py
@@ -1,7 +1,11 @@
 #! /usr/bin/python
 # -*- coding: cp1251 -*-
 # автор esguardian@outlook.com
-# верси€ 1.0.3
+# верси€ 1.0.4
+# ----------
+# 09.09.2015. вырезание данных из payload перенесено из SQL запроса в тело скрипта
+# так быстрее работает
+# -----------------------
 # ќтчет о действи€х с учетными запис€ми 
 # —обирает из базы данные плагина OSSEC о добавлении/удалении пользователей в группы
 # создании/удалении/блокировке/разблокировке учетных записей
@@ -10,6 +14,7 @@
 # и (желательно) с рабочих станций  
 #
 import sys
+import string
 import MySQLdb
 import codecs
 from datetime import date, timedelta
@@ -50,52 +55,116 @@ when = "timestamp between '" + starttime + "' and '" + endtime + "'"
 
 # Account change
 tabheader=u'\n\n\n»зменение учетных записей за период ' + startdate + ' - ' + enddate + '\n\n'
-what="userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, userdata8 as operator, username as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, substring_index(substring_index(data_payload,'.inbank.msk: ',-1),' Subject:',1) as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
-where="acid_event.plugin_id=7043 and (acid_event.plugin_sid=18110 or acid_event.plugin_sid=18112 or acid_event.plugin_sid=18142)"
-select="select  " + what + " where " + where + " and " + when + " order by time"
+what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, userdata8 as operator, username as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, data_payload as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
+where = "acid_event.plugin_id=7043 and (acid_event.plugin_sid=18110 or acid_event.plugin_sid=18112 or acid_event.plugin_sid=18142)"
+select = "select  " + what + " where " + where + " and " + when + " order by time"
 cursor.execute(select)
 with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
-     out.write(tabheader + colheader) 
-     row = cursor.fetchone() 
-     while row:
-         out.write(';'.join([str(c).replace(';',',').strip() for c in row]) + '\n')
-         row = cursor.fetchone()
+    out.write(tabheader + colheader) 
+    row = cursor.fetchone() 
+    while row:
+        outstr = row[0].replace(';',',').strip()
+        outstr = outstr + ';' + str(row[1]).replace(';',',').strip()
+        outstr = outstr + ';' + row[2].replace(';',',').strip()
+        outstr = outstr + ';' + row[3].replace(';',',').strip()
+        outstr = outstr + ';' + row[4].replace(';',',').strip()
+        # »звлекаем из payload строку с описанием операции
+        try:
+            info = row[5].split('.inbank.msk: ', 1)[-1].split('Subject:',1)[0]
+        except:
+            info = row[5]
+        outstr = outstr + ';' + info.replace(';',',').strip()
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
 out.close()
 # ---
 # global and universal group change
 tabheader=u'\n\n\n»зменение глобальных групп за период ' + startdate + ' - ' + enddate + '\n\n'
 
 with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
-     out.write(tabheader + colheader) 
-     # global group create
-     what ="userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, substring_index(substring_index(data_payload,'Group Name: ',-1),' Group',1) as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, substring_index(substring_index(data_payload,'.inbank.msk: ',-1),' Subject:',1) as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
-     where = "acid_event.plugin_id=7099 and acid_event.plugin_sid=18202"
-     select="select " + what + " where " + where + " and " + when  + " order by time"
-     cursor.execute(select)
-     row = cursor.fetchone() 
-     while row:
-         out.write(';'.join([str(c).replace(';',',').strip() for c in row]) + '\n')
-         row = cursor.fetchone()
-     # global group member add or remove and universal group member remove
-     what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, substring_index(substring_index(data_payload,'Group Name: ',-1),' Group',1) as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, substring_index(substring_index(data_payload,'CN=',-1),',OU=',1) as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
-     where = "acid_event.plugin_id=7107 and (acid_event.plugin_sid=18203 or acid_event.plugin_sid=18204 or acid_event.plugin_sid=18215)"
-     select="select " + what + " where " + where + " and " + when  + " order by time"
-     cursor.execute(select)
-     row = cursor.fetchone() 
-     while row:
-         out.write(';'.join([str(c).replace(';',',').strip() for c in row]) + '\n')
-         row = cursor.fetchone()
-     # Universal group member add 
-     # Ooops, ossim save this record in other format than "member removed" I think this is ossec agent bug
-     # so I need to do additional select
-     what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, substring_index(substring_index(substring_index(data_payload,'Group: Security ID:',-1),' Account Domain:',1),'Account Name: ',-1) as operator, substring_index(substring_index(data_payload,'Account Name: ',-1),' Account',1) as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, substring_index(substring_index(data_payload,'CN=',-1),',OU=',1) as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
-     where = "acid_event.plugin_id=7107 and acid_event.plugin_sid=18214"
-     select="select " + what + " where " + where + " and " + when  + " order by time"
-     cursor.execute(select)
-     row = cursor.fetchone() 
-     while row:
-         out.write(';'.join([str(c).replace(';',',').strip() for c in row]) + '\n')
-         row = cursor.fetchone()
+    out.write(tabheader + colheader) 
+    # global group create
+    what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, data_payload as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
+    where = "acid_event.plugin_id=7099 and acid_event.plugin_sid=18202"
+    select = "select " + what + " where " + where + " and " + when  + " order by time"
+    cursor.execute(select)
+    row = cursor.fetchone() 
+    while row:
+        outstr = row[0].replace(';',',').strip()
+        outstr = outstr + ';' + str(row[1]).replace(';',',').strip()
+        outstr = outstr + ';' + row[2].replace(';',',').strip()
+        # извлекаем название группы из payload
+        try:
+            object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
+        except:
+            object = 'None'
+        outstr = outstr + ';' + object.replace(';',',').strip()
+        outstr = outstr + ';' + row[3].replace(';',',').strip()
+        # извлекаем описание операции из payload, если не получитс€ запишем payload как есть
+        try:
+            info = row[4].split('.inbank.msk: ', 1)[-1].split('Subject:',1)[0]
+        except:
+            info = row[4]
+        outstr = outstr + ';' + info.replace(';',',').strip()
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
+        
+    # global group member add or remove and universal group member remove
+    what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, data_payload as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
+    where = "acid_event.plugin_id=7107 and (acid_event.plugin_sid=18203 or acid_event.plugin_sid=18204 or acid_event.plugin_sid=18215)"
+    select = "select " + what + " where " + where + " and " + when  + " order by time"
+    cursor.execute(select)
+    row = cursor.fetchone() 
+    while row:
+        outstr = row[0].replace(';',',').strip()
+        outstr = outstr + ';' + str(row[1]).replace(';',',').strip()
+        outstr = outstr + ';' + row[2].replace(';',',').strip()
+        # извлекаем название группы из payload
+        try:
+            object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
+        except:
+            object = 'None'
+        outstr = outstr + ';' + object.replace(';',',').strip()
+        outstr = outstr + ';' + row[3].replace(';',',').strip()
+        # извлекаем им€ пользовател€ из payload, если не получитс€ запишем payload как есть
+        try:
+            info = string.capwords(row[4].lower().split('cn=', 1)[-1].split(',ou=',1)[0])
+        except:
+            info = row[4]
+        outstr = outstr + ';' + info.replace(';',',').strip()
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
+    # Universal group member add 
+    # Ooops, ossim save this record in other format than "member removed" I think this is ossec agent bug
+    # so I need to do additional select
+    what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, data_payload as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
+    where = "acid_event.plugin_id=7107 and acid_event.plugin_sid=18214"
+    select = "select " + what + " where " + where + " and " + when  + " order by time"
+    cursor.execute(select)
+    row = cursor.fetchone() 
+    while row:
+        outstr = row[0].replace(';',',').strip()
+        outstr = outstr + ';' + str(row[1]).replace(';',',').strip()
+        try:
+            operator = row[3].split('Group: Security ID:',1)[-1].split(' Account Domain:',1)[0].split('Account Name: ')[-1]
+        except:
+            operator = 'None'
+        outstr = outstr + ';' + operator.replace(';',',').strip()
+        # извлекаем название группы из payload
+        try:
+            object = row[3].split('Account Domain',1)[-1].split('Account Name: ',1)[-1].split('Account Name: ',1)[-1].split(' Account',1)[0]
+        except:
+            object = 'None'
+        outstr = outstr + ';' + object.replace(';',',').strip()
+        outstr = outstr + ';' + row[2].replace(';',',').strip()
+        # извлекаем им€ пользовател€ из payload, если не получитс€ запишем payload как есть
+        try:
+            info = string.capwords(row[3].lower().split('cn=', 1)[-1].split(',ou=',1)[0])
+        except:
+            info = row[3]
+        outstr = outstr + ';' + info.replace(';',',').strip()
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
 out.close()
 # ---
 # Local group change
@@ -104,14 +173,30 @@ tabheader=u'\n\n\n»зменение локальных групп за период ' + startdate + ' - ' + en
 with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
      out.write(tabheader + colheader) 
      # in this cause ossec agent don't recognize account name by SID, so I use SID as "member"
-     what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, substring_index(substring_index(data_payload,'Group Name: ',-1),' Group',1) as object, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, substring_index(substring_index(data_payload,'Member: ',-1),' Account',1) as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
+     what = "userdata3 as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, inet_ntoa(conv(HEX(ip_src), 16, 10)) as source, data_payload as info from acid_event join extra_data on (acid_event.id=extra_data.event_id)"
      where = "acid_event.plugin_id=7107 and (acid_event.plugin_sid=18207 or acid_event.plugin_sid=18208)"
      select="select " + what + " where " + where + " and " + when  + " order by time"
      cursor.execute(select)
      row = cursor.fetchone() 
      while row:
-         out.write(';'.join([str(c).replace(';',',').strip() for c in row]) + '\n')
-         row = cursor.fetchone()
+        outstr = row[0].replace(';',',').strip()
+        outstr = outstr + ';' + str(row[1]).replace(';',',').strip()
+        outstr = outstr + ';' + row[2].replace(';',',').strip()
+        # извлекаем название группы из payload
+        try:
+            object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
+        except:
+            object = 'None'
+        outstr = outstr + ';' + object.replace(';',',').strip()
+        outstr = outstr + ';' + row[3].replace(';',',').strip()
+        # извлекаем им€ пользовател€ из payload, если не получитс€ запишем payload как есть
+        try:
+            info = row[4].split('Member: ', 1)[-1].split(' Account',1)[0]
+        except:
+            info = row[4]
+        outstr = outstr + ';' + info.replace(';',',').strip()
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
 out.close()
 # ---
 # --- End of All
