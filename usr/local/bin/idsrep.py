c2e51359527c88832ae00996fd6077929d0bfc26
Eugene Sokolov <esguardian@outlook.com>
2016-02-05 15:51:52 +0300

    В idsrep добавлена возможность фильтрации событий по AlienVault NIDS Plugin_SID, чтобы исключать из отчета события, которые не интересны.

--- usr/local/bin/idsrep.py
+++ usr/local/bin/idsrep.py
@@ -1,9 +1,12 @@
 #! /usr/bin/python
 # -*- coding: utf8 -*-
 # author Eugene Sokolov esguardian@outlook.com
-# version 2.0.1 + данные geoip 
+# version 2.0.1 
+# добавлен фильтр для исключения сигнатур.
+# Фильтр применять с осторожностью, каждая исключенная сигнатура может быть признаком атаки,
+# если вы ее отключили, используйте другой признак для детектирования аналогичной атаки.
 # команда: 
-# idsrep.py number host_group_name
+# idsrep.py l=number g=host_group_name f=/filepath/to/filter/file
 # где: 
 # number - число дней от "сегодня", если пропущено, то 1
 # host_group_name - строковое имя группы хостов (asset group) предварительно созданной в OSSIM
@@ -11,8 +14,15 @@
 # если имя не указано будут выданы события IDS (suricata) для всех хостов
 # будьте внимательны, в этом случае объем файла может быть слишком большим для Excel,
 # поскольку отчет будет содержать огромное количество неагрегированных данных netflow 
-#
-# 
+# /filepath/to/filter/file путь к файлу со списком фильтров событий.
+# формат записей в файле 
+# по одному номеру сигнатуры (plugin sid) в строке с комментарием после знака ':'
+# Например:
+# --------
+# 2011346 : комментарий можно по-русски но только в cp1251
+# 2012252 : ET SHELLCODE Common 0a0a0a0a Heap Spray String
+# ---------
+# Кодировка строк cp1251
 # Скрипт создает csv со списком событий IDS, а затем для каждого "атакующего" список всех его Netwlow, 
 # можно легко посмотреть, что происходило.
 # 
@@ -34,15 +44,31 @@ asset_dbschema='alienvault'
 # --- End of Database config
 
 # ---- Init 
-
 period=1
 asset_group_name=''
+filter_file_path=''
 if len(sys.argv) > 1:
     for c in sys.argv:
-        if c.isdigit():
-            period = int(c)
-        elif 'idsrep.py' not in c:
-            asset_group_name = c.strip('"').strip("'")
+        if 'l=' in c:
+            period = int(c.split('l=')[-1].strip())
+        elif 'g=' in c:
+            asset_group_name = c.split('g=')[-1].strip('"').strip("'").strip()
+        elif 'f=' in c:
+            filter_file_path = c.split('f=')[-1].strip('"').strip("'").strip()
+
+mytz="'+03:00'"
+mycharset='cp1251'
+dbcharset='utf8'
+
+filtered_sids = []
+
+if filter_file_path != "":
+    if os.path.isfile(filter_file_path):
+        with codecs.open(filter_file_path, 'r', encoding=mycharset) as f:
+            for line in f:
+                if ':' in line:
+                    filtered_sids.append(str(line.split(':')[0].strip()))
+        f.close()
 
 
 # set time interval for mySQL Select
@@ -126,31 +152,33 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
     out.write(tabheader + colheader) 
     row = cursor.fetchone() 
     while row:
-        src = row[2].strip()       
-        if row[4] is None:
-            if src in my_rep_data:
-                rep = my_rep_data[src]
+        src = row[2].strip() 
+        plugin_sid = str(row[0]).strip() 
+        if plugin_sid not in filtered_sids:
+            if row[4] is None:
+                if src in my_rep_data:
+                    rep = my_rep_data[src]
+                else:
+                    rep = 'None'
             else:
-                rep = 'None'
-        else:
-            rep = str(row[4]).decode(dbcharset) 
-        if rep.lower() != 'false':
-            place = get_place(reader, src, mycharset)
-            outstr = str(row[1]).decode(dbcharset).replace(';',',').strip()        
-            outstr = outstr + ';' + src
-            outstr = outstr + ';' + place
-            outstr = outstr + ';' + row[3].strip()
-            outstr = outstr + ';' + rep.replace(';',',')
-            plugin_sid = str(row[0]).strip() 
-            list.append(outstr)
-            # now get signature name
-            signature_name = u'Unknown'
-            cursor_av.execute('select name from plugin_sid where plugin_id=1001 and sid=' + plugin_sid)
-            row_av = cursor_av.fetchone()
-            for c in row_av:
-                signature_name = str(c).decode(dbcharset)
-            outstr = signature_name.replace(';',',') + ';' + outstr
-            out.write(outstr + '\n')
+                rep = str(row[4]).decode(dbcharset) 
+            if rep.lower() != 'false':
+                place = get_place(reader, src, mycharset)
+                outstr = str(row[1]).decode(dbcharset).replace(';',',').strip()        
+                outstr = outstr + ';' + src
+                outstr = outstr + ';' + place
+                outstr = outstr + ';' + row[3].strip()
+                outstr = outstr + ';' + rep.replace(';',',')
+                plugin_sid = str(row[0]).strip() 
+                list.append(outstr)
+                # now get signature name
+                signature_name = u'Unknown'
+                cursor_av.execute('select name from plugin_sid where plugin_id=1001 and sid=' + plugin_sid)
+                row_av = cursor_av.fetchone()
+                for c in row_av:
+                    signature_name = str(c).decode(dbcharset)
+                outstr = signature_name.replace(';',',') + ';' + outstr
+                out.write(outstr + '\n')
         row = cursor.fetchone()
     # and now add to the file netflow data for each event but deduplicate
     dup=[]
