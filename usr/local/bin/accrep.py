277511f4876374989954cf501bbd87044ee5ddf5
Eugene Sokolov <esguardian@outlook.com>
2016-05-11 17:39:38 +0300

    Изменен oramon.cfg. Изменен accrep.py, добавлен раздел об учетках Oracle.

--- usr/local/bin/accrep.py
+++ usr/local/bin/accrep.py
@@ -25,8 +25,10 @@ from OSSIM_helper import get_db_connection_data
 # Datababe connection config.
 (dbhost,dbuser,dbpass) = get_db_connection_data()
 dbshema='alienvault_siem'
+asset_dbschema='alienvault'
 # --- End of Database config
-
+def mystr (v,charset):
+    return str(v).decode(charset).replace(';',':').replace(',',':').strip()
 # ---- Init 
 
 period=1
@@ -65,17 +67,17 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
     out.write(tabheader + colheader) 
     row = cursor.fetchone() 
     while row:
-        outstr = str(row[0]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[1]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[2]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[3]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[4]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = unicode(row[0]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[1]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[2]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[3]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[4]).replace(';',':').replace(',',':').strip()
         # Извлекаем из payload строку с описанием операции
         try:
             info = row[5].split('.inbank.msk: ', 1)[-1].split('Subject:',1)[0]
         except:
             info = row[5]
-        outstr = outstr + ';' + str(info).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(info,dbcharset)
         out.write(outstr + '\n')
         row = cursor.fetchone()
 out.close()
@@ -92,22 +94,22 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
     cursor.execute(select)
     row = cursor.fetchone() 
     while row:
-        outstr = str(row[0]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[1]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[2]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = unicode(row[0]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[1]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[2]).replace(';',':').replace(',',':').strip()
         # извлекаем название группы из payload
         try:
             object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
         except:
             object = 'None'
-        outstr = outstr + ';' + str(object).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[3]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(object,dbcharset)
+        outstr = outstr + ';' + unicode(row[3]).replace(';',':').replace(',',':').strip()
         # извлекаем описание операции из payload, если не получится запишем payload как есть
         try:
             info = row[4].split('.inbank.msk: ', 1)[-1].split('Subject:',1)[0]
         except:
             info = row[4]
-        outstr = outstr + ';' + str(info).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(info,dbcharset)
         out.write(outstr + '\n')
         row = cursor.fetchone()
         
@@ -118,22 +120,22 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
     cursor.execute(select)
     row = cursor.fetchone() 
     while row:
-        outstr = str(row[0]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[1]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[2]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = unicode(row[0]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[1]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[2]).replace(';',':').replace(',',':').strip()
         # извлекаем название группы из payload
         try:
             object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
         except:
             object = 'None'
-        outstr = outstr + ';' + str(object).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[3]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(object,dbcharset)
+        outstr = outstr + ';' + unicode(row[3]).replace(';',':').replace(',',':').strip()
         # извлекаем имя пользователя из payload, если не получится запишем payload как есть
         try:
             info = string.capwords(row[4].lower().split('cn=', 1)[-1].split(',ou=',1)[0])
         except:
             info = row[4]
-        outstr = outstr + ';' + str(info).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(info,dbcharset)
         out.write(outstr + '\n')
         row = cursor.fetchone()
     # Universal group member add 
@@ -145,26 +147,26 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
     cursor.execute(select)
     row = cursor.fetchone() 
     while row:
-        outstr = str(row[0]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = unicode(row[0]).replace(';',':').replace(',',':').strip()
         outstr = outstr + ';' + str(row[1]).decode(dbcharset).replace(';',':').replace(',',':').strip()
         try:
             operator = row[3].split('Group: Security ID:',1)[-1].split(' Account Domain:',1)[0].split('Account Name: ')[-1]
         except:
             operator = 'None'
-        outstr = outstr + ';' + str(operator).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(operator,dbcharset)
         # извлекаем название группы из payload
         try:
             object = row[3].split('Account Domain',1)[-1].split('Account Name: ',1)[-1].split('Account Name: ',1)[-1].split(' Account',1)[0]
         except:
             object = 'None'
-        outstr = outstr + ';' + str(object).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[2]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(object,dbcharset)
+        outstr = outstr + ';' + unicode(row[2]).replace(';',':').replace(',',':').strip()
         # извлекаем имя пользователя из payload, если не получится запишем payload как есть
         try:
             info = string.capwords(row[3].lower().split('cn=', 1)[-1].split(',ou=',1)[0])
         except:
             info = row[3]
-        outstr = outstr + ';' + str(info).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(info,dbcharset)
         out.write(outstr + '\n')
         row = cursor.fetchone()
 out.close()
@@ -181,25 +183,60 @@ with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
      cursor.execute(select)
      row = cursor.fetchone() 
      while row:
-        outstr = str(row[0]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[1]).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[2]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = unicode(row[0]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[1]).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + unicode(row[2]).replace(';',':').replace(',',':').strip()
         # извлекаем название группы из payload
         try:
             object = row[4].split('Group Name: ',1)[-1].split('Group',1)[0]
         except:
             object = 'None'
-        outstr = outstr + ';' + str(object).decode(dbcharset).replace(';',':').replace(',',':').strip()
-        outstr = outstr + ';' + str(row[3]).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(object,dbcharset)
+        outstr = outstr + ';' + unicode(row[3]).replace(';',':').replace(',',':').strip()
         # извлекаем имя пользователя из payload, если не получится запишем payload как есть
         try:
             info = row[4].split('Member: ', 1)[-1].split(' Account',1)[0]
         except:
             info = row[4]
-        outstr = outstr + ';' + str(info).decode(dbcharset).replace(';',':').replace(',',':').strip()
+        outstr = outstr + ';' + mystr(info,dbcharset)
         out.write(outstr + '\n')
         row = cursor.fetchone()
 out.close()
 # ---
+# ORACLE Account change
+conn_av = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpass, db=asset_dbschema, charset=dbcharset)
+cursor_av = conn_av.cursor() 
+tabheader=u'\n\n\nИзменение учетных записей Oracle (АБС) за период ' + startdate + ' - ' + enddate + '\n\n'
+what = "plugin_sid as action, convert_tz(timestamp,'+00:00'," + mytz +") as time, username as operator, userdata3 as object, userdata1 as source, userdata4 as info from acid_event left outer join extra_data on (acid_event.id=extra_data.event_id)"
+where = "acid_event.plugin_id=9009 and (acid_event.plugin_sid=24 or acid_event.plugin_sid=26)"
+select = "select  " + what + " where " + where + " and " + when + " order by time"
+cursor.execute(select)
+with codecs.open(outfullpath, 'a', encoding=mycharset) as out:
+    out.write(tabheader + colheader) 
+    row = cursor.fetchone() 
+    while row:
+        # Получаем название сигнатуры
+        signature_name = u'Unknown'
+        cursor_av.execute("select name from plugin_sid where plugin_id=9009 and sid=%s", (row[0],))
+        row_av = cursor_av.fetchone()
+        signature_name = unicode(row_av[0])
+        
+        outstr = signature_name
+        outstr = outstr + ';' + unicode(row[1])
+        outstr = outstr + ';' + unicode(row[2])
+        if row[0] == 26:
+            grantee = unicode(row[5]).split('granted for')[-1].split('by')[0].strip()
+            outstr = outstr + ";" + grantee
+        else:
+            outstr = outstr + ';' + unicode(row[3])
+        outstr = outstr + ';' + unicode(row[4])
+        if row[0] == 26:
+            outstr = outstr + ';' + unicode(row[3])
+        else:
+            outstr = outstr + ';' + unicode(row[5])
+        out.write(outstr + '\n')
+        row = cursor.fetchone()
+out.close()
+
 # --- End of All
 conn.close()
